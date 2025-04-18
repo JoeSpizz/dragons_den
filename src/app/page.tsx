8ca388c5e85ba04393eb8bab4b649ab281f28d8d
JoeSpizz <spizjo01@gmail.com>
2025-04-18 17:08:50 -0400

    first draft of homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,26 @@ export default function Page() {
 
       <div className={styles.content}>
         <header className={styles.header}>
-          <h1 className={styles.headerTitle}>Dragon’s Den Games</h1>
+        <h1 className={styles.headerTitle}>
+  Dragon’s Den
+  {"\n"}
+  Games
+</h1>
         </header>
 
         <div className={styles.spacer} />
 
         <div className={styles.cards}>
           <Link href="/tcg" className={`${styles.card} ${styles.cardLarge}`}>
-            <Image src="/icons/tcg_logo.png" alt="TCG" width={80} height={80} />
+            <Image src="/icons/tcg_icon.png" alt="TCG" width={150} height={150} />
             <span>Trading Card Games</span>
           </Link>
-          <Link href="/minis" className={styles.card}>
-            <Image src="/icons/mini_logo.png" alt="Minis" width={64} height={64} />
+          <Link href="/minis" className={`${styles.card} ${styles.cardSmall}`} >
+            <Image src="/icons/mini_icon.png" alt="Minis" width={135} height={135} />
             <span>Tabletop Miniatures</span>
           </Link>
-          <Link href="/plushies" className={styles.card}>
-            <Image src="/icons/plushie_logo.png" alt="Plushies" width={64} height={64} />
+          <Link href="/plushies" className={`${styles.card} ${styles.cardSmall}`}>
+            <Image src="/icons/plushie_icon.png" alt="Plushies" width={135} height={135} />
             <span>Plushies</span>
           </Link>
         </div>
